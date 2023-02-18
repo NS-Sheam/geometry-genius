65807e47471eef62878d61aa6fd6cf63c762be05
NS-Sheam <123sheamfeni@gmail.com>
2023-02-18 16:21:54 +0600

    More two card javascript work done

--- js/utilies.js
+++ js/utilies.js
@@ -34,7 +34,7 @@ document.getElementById('triangle-btn').addEventListener('click', function (even
     const height = gentInputValue('triangle-height');
     const multiplyValue = multiplyingValueWithHalf(base, height);
     if (isNaN(base) || isNaN(height)) {
-        alert("Provide data correctly");
+        alert("Provide both data correctly");
         return;
     }
     serial+=1;
@@ -48,7 +48,7 @@ document.getElementById('rectangle-btn').addEventListener('click', function (eve
     const length = gentInputValue('rectangle-length');
     const multiplyValue = multiply(width, length);
     if (isNaN(width) || isNaN(length)) {
-        alert("Provide data correctly");
+        alert("Provide both data correctly");
         return;
     }
     serial+=1;
@@ -57,28 +57,14 @@ document.getElementById('rectangle-btn').addEventListener('click', function (eve
     document.getElementById('rectangle-length').value = '';
     
 });
-document.getElementById('pentagon-btn').addEventListener('click', function (event) {
-    const shapeName = event.target.parentNode.children[1].innerText;
-    const perimeter = gentInputValue('perimeter');
-    const apothem = gentInputValue('apothem');
-    const multiplyValue = multiply(perimeter, apothem);
-    if (isNaN(perimeter) || isNaN(apothem)) {
-        alert("Provide data correctly");
-        return;
-    }
-    serial+=1;
-    addOnCalcutionArea(shapeName, multiplyValue, serial);
-    document.getElementById('perimeter').value = '';
-    document.getElementById('apothem').value = '';
-    
-});
+
 document.getElementById('parallelogram-btn').addEventListener('click', function (event) {
     const shapeName = event.target.parentNode.children[1].innerText;
     const base = gentInputValue('parallelogram-base');
     const height = gentInputValue('parallelogram-height');
     const multiplyValue = multiply(base, height);
     if (isNaN(base) || isNaN(height)) {
-        alert("Provide data correctly");
+        alert("Provide both data correctly");
         return;
     }
     serial+=1;
@@ -92,13 +78,43 @@ document.getElementById('rhombus-btn').addEventListener('click', function (event
     const diagonal2 = gentInputValue('diagonal2');
     const multiplyValue = multiplyingValueWithHalf(diagonal1, diagonal2);
     if (isNaN(diagonal1) || isNaN(diagonal2)) {
-        alert("Provide data correctly");
+        alert("Provide both data correctly");
         return;
     }
     serial+=1;
     addOnCalcutionArea(shapeName, multiplyValue, serial);
     document.getElementById('diagonal1').value = '';
     document.getElementById('diagonal2').value = '';
+});
+document.getElementById('pentagon-btn').addEventListener('click', function (event) {
+    const shapeName = event.target.parentNode.children[1].innerText;
+    const perimeter = gentInputValue('perimeter');
+    const apothem = gentInputValue('apothem');
+    const multiplyValue = multiply(perimeter, apothem);
+    if (isNaN(perimeter) || isNaN(apothem)) {
+        alert("Provide both data correctly");
+        return;
+    }
+    serial+=1;
+    addOnCalcutionArea(shapeName, multiplyValue, serial);
+    document.getElementById('perimeter').value = '';
+    document.getElementById('apothem').value = '';
+    
+});
+document.getElementById('ellipse-btn').addEventListener('click', function (event) {
+    const shapeName = event.target.parentNode.children[1].innerText;
+    const axisA = gentInputValue('axis-a');
+    const axisB = gentInputValue('axis-b');
+    const multiplyValue = (Math.PI * axisA * axisB).toFixed(2);
+    if (isNaN(axisA) || isNaN(axisB)) {
+        alert("Provide both data correctly");
+        return;
+    }
+    serial+=1;
+    addOnCalcutionArea(shapeName, multiplyValue, serial);
+    document.getElementById('axis-a').value = '';
+    document.getElementById('axis-b').value = '';
+    
 });
 
 
